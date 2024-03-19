8e14b2e13aa373dfda1693e825105cb858916181
Achanti Vamsi Krishna <151735259+vkchnati@users.noreply.github.com>
2024-03-19 12:55:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-  const downloadGWHButton = document.getElementById("downloadGWH");
-  const downloadMEFAButton = document.getElementById("downloadMEFA");
+	const downloadGWHButton = document.getElementById("downloadGWH");
+	const downloadMEFAButton = document.getElementById("downloadMEFA");
 	const downloadFMOHButton = document.getElementById("downloadFMOH");
 	const downloadAPEButton = document.getElementById("downloadAPE");
 	const downloadDFEButton = document.getElementById("downloadDFE");
@@ -20,60 +20,60 @@
 	const downloadMMIButton = document.getElementById("downloadMMI");
 	const downloadORButton = document.getElementById("downloadOR");
 	const downloadPHEButton = document.getElementById("downloadPHE");
-  const downloadPSMButton = document.getElementById("downloadPSM");
-  const downloadSWHButton = document.getElementById("downloadSWH");
-  const downloadSURVEYButton = document.getElementById("downloadSURVEY");
-  const downloadSWCEButton = document.getElementById("downloadSWCE");
-  const downloadTDRSButton = document.getElementById("downloadTDRS");
-  const downloadTOMButton = document.getElementById("downloadTOM");
-  const downloadTOSButton = document.getElementById("downloadTOS");
-  const downloadWATERButton = document.getElementById("downloadWATER");
+	const downloadPSMButton = document.getElementById("downloadPSM");
+	const downloadSWHButton = document.getElementById("downloadSWH");
+	const downloadSURVEYButton = document.getElementById("downloadSURVEY");
+	const downloadSWCEButton = document.getElementById("downloadSWCE");
+	const downloadTDRSButton = document.getElementById("downloadTDRS");
+	const downloadTOMButton = document.getElementById("downloadTOM");
+	const downloadTOSButton = document.getElementById("downloadTOS");
+	const downloadWATERButton = document.getElementById("downloadWATER");
 	const downloadESButton = document.getElementById("downloadES");
 	const downloadAETBMButton = document.getElementById("downloadAETBM");
 	const downloadPVTSButton = document.getElementById("downloadPVTS");
-	const downloadDAMButton = document.getElementById("downloadDAM");
+        const downloadDAMButton = document.getElementById("downloadDAM");
 	const downloadFPDMButton = document.getElementById("downloadFPDM");
 	const downloadAIBUButton = document.getElementById("downloadAIBU");
 	
 	
 
     const fileUrls = {
-      GWH: "C:\\Users\\hp\\Desktop\\AGR\\GWH.pdf",
-	  ES: "C:\\Users\\hp\\Desktop\\AGR\\Electrical Systems.pdf",
-      MEFA: "C:\\Users\\hp\\Desktop\\AGR\\MEFA  ALL.pdf",
-	  FMOH: "C:\\Users\\hp\\Desktop\\AGR\\FMOH.pdf",
-	  APE:"C:\\Users\\hp\\Desktop\\AGR\\Agricultural Process Engineering.pdf",
-	  DFE: "C:\\Users\\hp\\Desktop\\AGR\\DAIRY AND FOOD ENGINEERING.pdf",
-	  DSWCFS: "C:\\Users\\hp\\Desktop\\AGR\\DSWCFS.pdf",
-	  EPBM: "C:\\Users\\hp\\Desktop\\AGR\\EPBM.pdf",
-	  FMTS: "C:\\Users\\hp\\Desktop\\AGR\\Farm Power and Tractor Systems.pdf",
-	  FMEI: "C:\\Users\\hp\\Desktop\\AGR\\FME-I.pdf",
-		  FMEII: "C:\\Users\\hp\\Desktop\\AGR\\FME-II.pdf",
-	  FOODPACK: "C:\\Users\\hp\\Desktop\\AGR\\Food Packaging.pdf",
-	  FQC: "C:\\Users\\hp\\Desktop\\AGR\\Food Quality & Control.pdf",
-	  GIS: "C:\\Users\\hp\\Desktop\\AGR\\GIS & REMOTE SENSING.pdf",
-	  GHT: "C:\\Users\\hp\\Desktop\\AGR\\GREEN HOUSE TECHNOLOGY.pdf",
-	  HMT: "C:\\Users\\hp\\Desktop\\AGR\\HEAT AND MASS TRANSFER.pdf",
-	  HES: "C:\\Users\\hp\\Desktop\\AGR\\HES.pdf",
-	  IDE: "C:\\Users\\hp\\Desktop\\AGR\\Irrigation&Drainage Engineering.pdf",
-	  MIII: "C:\\Users\\hp\\Desktop\\AGR\\Mathematics-III.pdf",
-	  MI: "C:\\Users\\hp\\Desktop\\AGR\\MICRO IRRIGATION.pdf",
-	  MMI: "C:\\Users\\hp\\Desktop\\AGR\\MMI.pdf",
-	  OR: "C:\\Users\\hp\\Desktop\\AGR\\OPERATION RESEARCH.pdf",
-	  PHE: "C:\\Users\\hp\\Desktop\\AGR\\PHE&HP.pdf",
-	  PSM: "C:\\Users\\hp\\Desktop\\AGR\\PSM.pdf",
-	  SWH: "C:\\Users\\hp\\Desktop\\AGR\\SURFACE WATER HYDROLOGY.pdf",
-	  SURVEY: "C:\\Users\\hp\\Desktop\\AGR\\SURVEYING.pdf",
-	  SWCE: "C:\\Users\\hp\\Desktop\\AGR\\SWCE MATERIAL.pdf",
-	  TDRS: "C:\\Users\\hp\\Desktop\\AGR\\TD&RS MATERIAL.pdf",
-	  TOM: "C:\\Users\\hp\\Desktop\\AGR\\THEORY OF MACHINES.pdf",
-	  TOS: "C:\\Users\\hp\\Desktop\\AGR\\THEORY OF STRUCTURES.pdf",
-	  WATER: "C:\\Users\\hp\\Desktop\\AGR\\Watershed Management.pdf",
-	  AETBM: "C:\\Users\\hp\\Desktop\\AGR\\AET&BM.pdf",
-	  FPDM: "C:\\Users\\hp\\Desktop\\AGR\\FPD&M.pdf",
-	  AIBU: "C:\\Users\\hp\\Desktop\\AGR\\AI&BU.pdf",
-	  PVTS: "C:\\Users\\hp\\Desktop\\AGR\\PVT&S.pdf",
-	  DAM: "C:\\Users\\hp\\Desktop\\AGR\\DAM.pdf"
+          GWH: "GWH.pdf",
+	  ES: "Electrical Systems.pdf",
+          MEFA: "MEFA  ALL.pdf",
+	  FMOH: "FMOH.pdf",
+	  APE:"Agricultural Process Engineering.pdf",
+	  DFE: "DAIRY AND FOOD ENGINEERING.pdf",
+	  DSWCFS: "DSWCFS.pdf",
+	  EPBM: "EPBM.pdf",
+	  FMTS: "Farm Power and Tractor Systems.pdf",
+	  FMEI: "FME-I.pdf",
+          FMEII: "FME-II.pdf",
+	  FOODPACK: "Food Packaging.pdf",
+	  FQC: "Food Quality & Control.pdf",
+	  GIS: "GIS & REMOTE SENSING.pdf",
+	  GHT: "GREEN HOUSE TECHNOLOGY.pdf",
+	  HMT: "HEAT AND MASS TRANSFER.pdf",
+	  HES: "HES.pdf",
+	  IDE: "Irrigation&Drainage Engineering.pdf",
+	  MIII: "Mathematics-III.pdf",
+	  MI: "MICRO IRRIGATION.pdf",
+	  MMI: "MMI.pdf",
+	  OR: "OPERATION RESEARCH.pdf",
+	  PHE: "PHE&HP.pdf",
+	  PSM: "PSM.pdf",
+	  SWH: "SURFACE WATER HYDROLOGY.pdf",
+	  SURVEY: "SURVEYING.pdf",
+	  SWCE: "SWCE MATERIAL.pdf",
+	  TDRS: "TD&RS MATERIAL.pdf",
+	  TOM: "THEORY OF MACHINES.pdf",
+	  TOS: "THEORY OF STRUCTURES.pdf",
+	  WATER: "Watershed Management.pdf",
+	  AETBM: "AET&BM.pdf",
+	  FPDM: "FPD&M.pdf",
+	  AIBU: "AI&BU.pdf",
+	  DAM: "DAM.pdf",
+	  PVTS: "PVT&S.pdf"
     };
 
     downloadGWHButton.addEventListener("click", () => {
@@ -177,10 +177,9 @@
 	downloadPVTSButton.addEventListener("click", () => {
       downloadFile(fileUrls.PVTS, "PVT&S.pdf");
     });
-	downloadDAMButton.addEventListener("click", () => {
+downloadDAMButton.addEventListener("click", () => {
       downloadFile(fileUrls.DAM, "DAM.pdf");
     });
-
 	downloadFPDMButton.addEventListener("click", () => {
       downloadFile(fileUrls.FPDM, "FPD&M.pdf");
     });
@@ -197,45 +196,47 @@
       link.download = fileName;
       link.click();
     }
- 
-// Function to show materials section
-function showMaterials() {
-    document.getElementById('home-details').style.display = 'none';
-    document.getElementById('materials-list').style.display = 'block';
-    document.getElementById('contact-details').style.display = 'none';
-    document.getElementById('quiz-details').style.display = 'none';
-}
+  
+	function showMaterials() {
+    var homeDetails = document.getElementById('home-details');
+    homeDetails.style.display = 'none';
 
-// Function to show home section
-function showHome() {
-    document.getElementById('home-details').style.display = 'block';
-    document.getElementById('materials-list').style.display = 'none';
-    document.getElementById('contact-details').style.display = 'none';
-    document.getElementById('quiz-details').style.display = 'none';
-}
+    var materialsList = document.getElementById('materials-list');
+    materialsList.style.display = 'block';
 
-// Function to open quiz section
-function openQuiz() {
-    document.getElementById('quiz-details').style.display = 'block';
-    document.getElementById('home-details').style.display = 'none';
-    document.getElementById('materials-list').style.display = 'none';
-    document.getElementById('contact-details').style.display = 'none';
-}
+	var contactDetails = document.getElementById('contact-details');
+      contactDetails.style.display = 'none';
+  }
 
-// Function to show contact details
+  function showHome() {
+    var homeDetails = document.getElementById('home-details');
+    homeDetails.style.display = 'block';
+
+    var materialsList = document.getElementById('materials-list');
+    materialsList.style.display = 'none';
+
+	 var contactDetails = document.getElementById('contact-details');
+      contactDetails.style.display = 'none';
+  }
+  // Open materials in new tabs
+  const materialLinks = document.querySelectorAll('#materials-list li a');
+  for (const link of materialLinks) {
+    link.target = '_blank';
+  }
+
+  // Contact
 function showContactDetails() {
-    document.getElementById('contact-details').style.display = 'block';
-    document.getElementById('home-details').style.display = 'none';
-    document.getElementById('materials-list').style.display = 'none';
-    document.getElementById('quiz-details').style.display = 'none';
-}
+      var contactDetails = document.getElementById('contact-details');
+      contactDetails.style.display = 'block';
 
-// Activate the home link to trigger showHome()
-document.querySelector('nav a[href="#"]').addEventListener('click', showHome);
+      var homeDetails = document.getElementById('home-details');
+      homeDetails.style.display = 'none';
 
-// Activate the contact link to trigger showContactDetails()
-document.querySelector('nav a[href="#c"]').addEventListener('click', showContactDetails);
-
+      var materialsList = document.getElementById('materials-list');
+      materialsList.style.display = 'none';
+    }
+  // Activate the home link to trigger showHome()
+  document.querySelector('nav a[href="#"]').addEventListener('click', showHome);
 
  function searchMaterials() {
   const searchTerm = document.getElementById("search-bar").value.toLowerCase();
