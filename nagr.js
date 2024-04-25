b5180b8495f5d446ac5d016cccd3471f00e3215e
Achanti Vamsi Krishna <151735259+vkchnati@users.noreply.github.com>
2024-04-25 11:04:22 +0530

    Update nagr.js

--- nagr.js
+++ nagr.js
@@ -405,7 +405,7 @@ function showbtech() {
   var securityCode = prompt("Enter the security code:");
 
   // Validate user ID and security code
-  if ((userId === "vkachanti" && securityCode === "agriengg@1") || (userId === "saida" && securityCode === "96403")) {
+  if ((userId === "vkachanti" && securityCode === "agriengg@1") || (userId === "saida" && securityCode === "96403")|| (userId === "8309127255" && securityCode === "83091")) {
     // If both are correct, show the materials list
     document.getElementById("materials-list").style.display = "block";
     document.getElementById("btechlist").style.display = "block";
