9b436f0a0569e3b3e489b47cdd81c406fc9e0332
Achanti Vamsi Krishna <151735259+vkchnati@users.noreply.github.com>
2024-05-16 20:36:52 +0530

    Update nagr.js

--- nagr.js
+++ nagr.js
@@ -452,7 +452,8 @@ function validateLogin() {
   if ((userId === "vkachanti" && securityCode === "agriengg@")|| (userId === "saida" && securityCode === "96403") || 
         (userId === "8309127255" && securityCode === "83091") ||
       (userId === "19JQ1A3528" && securityCode === "12345")||(userId === "Mohith8472" && securityCode === "Chandana@123")||
-       (userId === "7993979558" && securityCode === "79939") || (userId === "Aravind" && securityCode === "Aravind@123")) {
+       (userId === "7993979558" && securityCode === "79939") || (userId === "Aravind" && securityCode === "Aravind@123")||(userId === "7993979558" && securityCode === "79939") || 
+       (userId === "Prasad@2004" && securityCode === "Chinni@143")) {
     // If login succeeds, display the B.Tech materials list
     showBtechList();
     document.getElementById("login-message").style.display = "none";
