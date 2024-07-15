b4592e6fef496273d36d8e8074e037a75ab63efa
Achanti Vamsi Krishna <151735259+vkchnati@users.noreply.github.com>
2024-07-15 10:12:03 +0530

    Update nagr.js

--- nagr.js
+++ nagr.js
@@ -1,3 +1,12 @@
+function showSidebar(){
+      const sidebar = document.querySelector('.sidebar')
+      sidebar.style.display = 'flex'
+    }
+    function hideSidebar(){
+      const sidebar = document.querySelector('.sidebar')
+      sidebar.style.display = 'none'
+    }
+
 const downloadGWHButton = document.getElementById("downloadGWH");
 const downloadMEFAButton = document.getElementById("downloadMEFA");
 const downloadFMOHButton = document.getElementById("downloadFMOH");
